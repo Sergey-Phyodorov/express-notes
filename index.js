3f7f518531b843bb5850f0bfd9a823761cf7f0f8
Phyodorov S.V. <mail@phyodorov.ru>
2024-02-23 15:21:42 +0900

    Downgrade chalk version and setup simple HTTP server
    The chalk dependency version was downgraded from 5.3.0 to 4.1.2 for better compatibility, the package-lock.json has also been updated to reflect this. Additionally, a basic HTTP server was implemented in the index.js file by using the 'http' and 'fs' Node.js built-in modules along with the 'chalk' library for better console logging.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+const http = require('http');
+const fs = require('fs/promises');
+const chalk = require('chalk');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(chalk.bgBlue('Mетод запроса:', req.method));
+    console.log(chalk.blueBright('URL-адрес запроса:', req.url));
+
+    res.end('Hello from the server');
+});
+
+server.listen(port, () => {
+    console.log(chalk.green(`Server is running on port ${port} ...`));
+});
